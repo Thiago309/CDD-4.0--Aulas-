cdf70397206c78c7e2c37216122d08e299a36af9
Thiago_V[309] <116599158+Thiago309@users.noreply.github.com>
2023-09-27 17:04:55 -0300

    Update Exercicio_01.py

--- Codigos_Python/AULA 08/Exercicio_01.py
+++ Codigos_Python/AULA 08/Exercicio_01.py
@@ -2,9 +2,9 @@ anosIdade = int(input("Informe sua idade (Anos): "))
 mesesIdade = int(input("Informe sua idade (Meses): "))
 diasIdade = int(input("Informe sua idade (Dias): "))
 
-idadeDias = diasIdade + 365 * anosIdade + 30 * mesesIdade
+totalDias = diasIdade + 365 * anosIdade + 30 * mesesIdade
 
-print(f"Você têm {idadeDias} dias de vida.")
+print(f"Você têm {totalDias} dias de vida.")
 
 
 
