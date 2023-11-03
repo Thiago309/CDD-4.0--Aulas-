2f62901c3ed27b3049ab3baa4b65935041887b61
Thiago V. B. dos Santos <116599158+Thiago309@users.noreply.github.com>
2023-11-02 23:18:57 -0300

    FIM

--- Codigos_Python/AULA 14 POO/BANK.py
+++ Codigos_Python/AULA 14 POO/BANK.py
@@ -2,46 +2,76 @@ class conta_bancaria:
     def __init__(self, n_conta, titular, tipo_conta):
         self.n_conta = n_conta
         self.saldo = 0.0
+        self.dvdaCredEsp = 0.0
         self.titular = titular
         self.tipo_conta = tipo_conta
         self.status = False
-        self.limite = 500.00
+        self.cred_especial = 500.00
 
     def depositar(self, valor_d):
         if self.status == True:
-            self.saldo += valor_d
-            return self.saldo
+            if self.dvdaCredEsp < 0:
+                self.dvdaCredEsp += valor_d
+            else:
+                self.saldo += valor_d
+                print(self.saldo)
+                return self.saldo
         else:
-            print("Conta inativa, se direcione ao centro bancário mais próximo da sua residencia\n")
+            print("Conta inativa, se direcione ao centro bancário mais próximo da sua residencia ")
             print("para ativar sua conta.")
 
     def sacar(self, valor_s):
         if self.status == True and self.saldo > 0:
             self.saldo -= valor_s
+            print(self.saldo)
             return self.saldo
         elif self.saldo == 0:
             print("Não há valor em sua conta, por favor deposite...")
+            print(self.saldo)
         else:
-            print("Conta inativa, se direcione ao centro bancário mais próximo da sua residencia\n")
+            print("Conta inativa, se direcione ao centro bancário mais próximo da sua residencia")
             print("para ativar sua conta.")
+            print(self.saldo)
 
     def verifSaldo(self):
         if self.status == True:
             print(f"Seu saldo é R${self.saldo:.2f}")
+            print(f"Seu saldo de credito especial é R${self.cred_especial:.2f}")
         else:
-            print("Conta inativa, se direcione ao centro bancário mais próximo da sua residencia\n")
+            print("Conta inativa, se direcione ao centro bancário mais próximo da sua residencia")
             print("para ativar sua conta.")
 
     def onConta(self):
         if self.status == False:
-            self.satus == True
+            self.status = True
             print("Conta Ativada! Seja bem vindo a nossa família !!!")
+            print(self.saldo)
         else:
             print("A conta já encontra-se ativa !")
 
     def offConta(self):
         if self.status == True:
-            self.status == False
+            self.status = False
             print("Conta Desativada! Iremos sentir sua falta, volte novamente! =(")
+            print(self.saldo)
         else:
             print("A conta já encontra-se desativada !")
+
+    def aumLimite(self, CredEsp):
+        if self.status == True:
+            print(f"Seu saldo atual de Credito Especial é: {self.cred_especial:.2f}.")
+            print("Informe o valor o valor que você deseja solicitar para aumentar seu credito.")
+            self.cred_especial += CredEsp
+            return self.cred_especial
+        else:
+            print("Conta inativa, se direcione ao centro bancário mais próximo da sua residencia")
+            print("para ativar sua conta.")
+
+    def credEspecial(self, vCred):
+        if self.status == True:
+            if self.saldo < 0 and vCred <= self.cred_especial:
+                self.dvdaCredEsp = (self.dvdaCredEsp + vCred) * -1
+                return self.dvdaCredEsp
+        else:
+            print("Conta inativa, se direcione ao centro bancário mais próximo da sua residencia")
+            print("para ativar sua conta.")
--- Codigos_Python/AULA 14 POO/main.py
+++ Codigos_Python/AULA 14 POO/main.py
@@ -1,32 +1,37 @@
 from BANK import *
 
-opc = "n"
-C1 = conta_bancaria("98664", "Jhonny", "Corrente")
+opc = "s"
 
+C1 = conta_bancaria("98664", "Jhonny", "Corrente")
 while opc in "sS":
-    opc = input('''Informe a opção que deseja utilizar:
+    opc2 = int(input('''Informe a opção que deseja utilizar:
                     1 - Depositar;
                     2 - Sacar;
                     3 - Verificar o Saldo;
                     4 - Ativ. a Conta;
-                    5 - Desativ. a Conta.''')
+                    5 - Desativ. a Conta; 
+                    6 - Aum. Limite Cred. Especial;
+                    7 - Usar Cred. Especial;
+                    8 - Sair.\n
+                        Informe: '''))
 
-    if opc == 1:
-        valor_d = float(input("Informe o valor que você deseja depositar em R$:"))
+    if opc2 == 1:
+        valor_d = float(input("Informe o valor que você deseja depositar em R$: "))
         C1.depositar(valor_d)
-
-    elif opc == 2:
+    elif opc2 == 2:
         valor_s = float(input("Informe o valor que você deseja sacar em R$: "))
         C1.sacar(valor_s)
-
-    elif opc == 3:
+    elif opc2 == 3:
         C1.verifSaldo()
-
-    elif opc == 4:
+    elif opc2 == 4:
         C1.onConta()
-
-    elif opc == 5:
+    elif opc2 == 5:
         C1.offConta()
-
-
-    opc = input("Ainda deseja continuar a utilizar nossos serviços (s/n) ?")
+    elif opc2 == 6:
+        CredEsp = float(input("Informe o valor que você deseja solicitar para o aumento do limite: "))
+        C1.aumLimite(CredEsp)
+    elif opc2 == 7:
+        vCred = float(input("Informe o valor do credito especial que você deseja usar: "))
+        C1.credEspecial(vCred)
+    elif opc2 == 8:
+        opc = input("Ainda deseja continuar a utilizar nossos serviços (s/n)? ")
