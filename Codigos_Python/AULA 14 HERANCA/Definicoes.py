6aae6f0c46bea306fb4d2d90ee17121b92f16962
Thiago_V[309] <116599158+Thiago309@users.noreply.github.com>
2023-11-10 15:37:06 -0300

    10/11 definicoes

---
+++ Codigos_Python/AULA 14 HERANCA/Definicoes.py
@@ -0,0 +1,7 @@
+'''
+Entidade: É alguem ou algo que será trazido para a modelagem conceitual.
+Atributos: São as caracteristicas de uma determinada entidade.
+Relacionamento: São as ações que serão realizadas entre duas entidades.
+Chave Primaria: Codigo que identifica unicamente uma entidade em uma tabela.
+Chave Secundaria: Codigo que identifica unicamente um determinado objeto quando for unido em outra tabela.
+'''
