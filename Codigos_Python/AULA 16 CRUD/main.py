5d814e871af0b625b73bb0e4fccd1fa699bec64a
Thiago V. B. dos Santos <116599158+Thiago309@users.noreply.github.com>
2023-11-15 15:45:56 -0300

    15/11

--- Codigos_Python/AULA 16 CRUD/main.py
+++ Codigos_Python/AULA 16 CRUD/main.py
@@ -16,11 +16,22 @@ resultado = meucursor.fetchall()  # fetchall recebe tudo da pesquisa e retorna a
 for e in resultado:
     print(e)
 
-nome1 = input("Informe o nome que deseja adicionar na Base de dados: ")
-telefone1 = int(input("Informe o telefone que voce deseja adicionar na Base de Dados: "))
-sql = "insert into alunos (nome, telefone) values (%s, %s)"
-data = (nome1, telefone1)
-meucursor.execute(sql, data)
+# nome1 = input("Informe o nome que deseja adicionar na Base de dados: ")
+# telefone1 = int(input("Informe o telefone que voce deseja adicionar na Base de Dados: "))
+# sql = "insert into alunos (nome, telefone) values (%s, %s)"
+# data = (nome1, telefone1)
+# meucursor.execute(sql, data)
+# banco.commit()
+tiposexo = input("Informe o tipo sexual (M/F) do aluno(a): ")
+sql = "alter table alunos add colunmn sexo;"
+sql1 = "insert into alunos (sexo) values (%s)"
+data = (tiposexo)
+meucursor.execute(sql,sql1)
+tiposexo1 = input("Informe o tipo sexual (M/F) do funcionario(a): ")
+sql = "alter table func add colunmn sexo;"
+sql1 = "insert into func (sexo) values (%s)"
+data = (tiposexo1)
+meucursor.execute(sql,sql1)
 banco.commit()
 # userid = meucursor.lastrowid
 # print(userid)
